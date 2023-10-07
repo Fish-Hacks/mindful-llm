37c02e925030933c2b1547bc6a26e0925fb52018
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-10-07 13:14:47 +0800

    Add staticmethod to convert json data to emotion instance.

--- domain/emotion.py
+++ domain/emotion.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
+from json import loads
+from typing import Optional, List
 
 
 @dataclass
@@ -12,7 +13,27 @@ class Emotion:
         emotion (str, optional): The type of emotion. Defaults to "No Emotion".
         description (Optional[str], optional): A description or reason for the emotion. 
                                                Defaults to "No Description".
+
+    Methods:
+        from_json: Parses a JSON string and returns a list of Emotion instances.
     '''
+
     date: str
     emotion: str = "No Emotion"
     description: Optional[str] = "No Description"
+
+    @staticmethod
+    def from_json(json: str) -> List['Emotion']:
+        '''
+        Parses a JSON string to create a list of Emotion instances.
+
+        Args:
+            json (str): A JSON string representation of a list of emotions. Each emotion should have attributes 
+                        that match the Emotion class.
+
+        Returns:
+            List[Emotion]: A list of Emotion instances.
+        '''
+        data = loads(json)
+
+        return [Emotion(**entry) for entry in data]
